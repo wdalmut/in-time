101eea3a7af7d3c546b6a8139a718494817a1b07
Walter Dal Mut <walter.dalmut@gmail.com>
2017-01-14 10:01:32 +0100

    Fixes isBefore error in a particular time range

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var toTime = function(time) {
-  return time.split(':').reduce((memo, item, index) => (index === 0) ? memo + (item * 60) : memo + item);
+  return time.split(':').reduce((memo, item, index) => (index === 0) ? memo + (item * 60) : memo + item, 0);
 };
 
 var Time  = function(time) {
--- spec/index_spec.js
+++ spec/index_spec.js
@@ -5,7 +5,15 @@ describe("Time", () => {
     expect(time("10:00").isBefore('10:01')).toBe(true);
   });
 
+  it("is before without leading 0", () => {
+    expect(time("9:00").isBefore('10:01')).toBe(true);
+  });
+
   it("is not before", () => {
     expect(time("10:00").isBefore('09:59')).toBe(false);
+  });
+
+  it("is not before", () => {
+    expect(time("10:00").isBefore('9:59')).toBe(false);
   });
 });
